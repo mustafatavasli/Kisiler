bd6dcc2ffb1bbe66b990b30a762e9524a052cc8c
mustafatavasli <musstafatavasli@gmail.com>
2024-07-23 09:35:54 +0300

    Initial Commit

---
+++ Kisiler/KisilerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KisilerApp.swift
+//  Kisiler
+//
+//  Created by Mustafa TAVASLI on 23.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct KisilerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
